1d5ff37851b14c8bbf426ed5ac6aaca352465fc0
jfrazier <johnfrazier1222@gmail.com>
2017-05-09 01:45:53 -0400

    slideshow updates

--- scripts.js
+++ scripts.js
@@ -1,6 +1,39 @@
 $('.logo').hide();
-$('.logo').fadeIn(4000);
+$('.logo').fadeIn(1000);
 
 $('.nav').hide();
-$('.nav').slideDown(2000);
+$('.nav').slideDown(1000);
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+};
+
+// var slideIndex = 0;
+// carousel();
+
+// function carousel() {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none"; 
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1} 
+//     x[slideIndex-1].style.display = "block"; 
+//     setTimeout(carousel,4000); 
+// };
 
