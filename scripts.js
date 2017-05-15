69040add4f5be772d4dcc4b8b2b24a4fa761c032
jfrazier <johnfrazier1222@gmail.com>
2017-05-14 23:42:14 -0400

    final artist page

--- scripts.js
+++ scripts.js
@@ -23,18 +23,18 @@ function showDivs(n) {
 };
 
 
-var slideIndex = 0;
-carousel();
+// var slideIndex = 0;
+// carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel,4000); 
-};
+// function carousel() {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none"; 
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1} 
+//     x[slideIndex-1].style.display = "block"; 
+//     setTimeout(carousel,4000); 
+// };
 
